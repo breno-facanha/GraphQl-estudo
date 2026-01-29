7385da9d5806067e38663410e0dbfe46905a6ae8
Breno Façanha <brenofacanha.ti@gmail.com>
2026-01-29 11:26:10 -0300

    add type Produto e resovlvers melhorUsuario

--- index.js
+++ index.js
@@ -3,20 +3,68 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import gql from "graphql-tag";
 
 const typeDefs = gql`
+  scalar DateAtual
+
   type Query {
     ola: String
+    hora: DateAtual
+    melhorUsuario: Usuario!
+    melhorProduto: Produto!
+  },
+  type Usuario {
+    id: Int
+    nome: String
+    email: String
+    salario: Float
+    vip: Boolean
+  },
+
+  type Produto {
+    id: Int
+    nome: String
+    preco: Float
+    desconto: Float
+    precoComDesconto: Float 
   }
 `;
 
 const resolvers = {
   Query: {
-    ola: () => "Olá, mundo!",
+    ola: () => "Olá !",
+    hora: () => new Date().toLocaleString(),
+    melhorUsuario: () => {
+        return {
+            id: 1,
+            nome_completo: "Breno Facanha",
+            email: "breno@gmail.com",
+            salario: 4500.50,
+            vip: true
+        }
+    },
+    melhorProduto: () => {
+        return {
+            id: 1,
+            nome: "Smartphone",
+            preco: 2500.00,
+            desconto: 0.10
+        }
+    }
   },
+  Usuario: {
+    nome(usuario){
+      console.log(usuario);
+      return usuario.nome_completo;
+    }
+  },
+  Produto: {
+    precoComDesconto(produto){
+      const desconto = produto.preco * produto.desconto;
+      return produto.preco - desconto;
+    }
+  }
 };
 
-
-
-const servidor = new ApolloServer({typeDefs, resolvers});
+const servidor = new ApolloServer({ typeDefs, resolvers });
 const { url } = await startStandaloneServer(servidor);
 
 console.log(`Servidor rodando em: ${url}`);
